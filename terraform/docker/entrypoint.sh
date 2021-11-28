45bbb12ad286ad3e8601565f58ff43ca933b671e
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-11-28 11:30:30 +0100

    Replace docker compose with plain docker on terraform

--- terraform/docker/entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Disable strict host key checking on GitHub
-cd terraform && terraform init && terraform plan -out /tmp/plan -var-file="variables.tfvars"
-#&& terraform apply /tmp/plan
