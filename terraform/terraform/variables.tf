95a0ed9ce6bf49c3197763adc9c32d39bdcef061
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2022-09-26 15:27:42 +0200

    Restore DNS management

--- terraform/terraform/variables.tf
+++ terraform/terraform/variables.tf
@@ -7,3 +7,26 @@ variable "aws_credentials_file" {
   type = string
   description = "Path for the file containing aws credentials"
 }
+
+variable "do_token" {
+  type = string
+  description = "Token for Digital Ocean"
+}
+
+variable "domain_name" {
+  type = string
+  description = "Your domain name"
+}
+
+variable "old_bastion_ip" {
+  type = string
+  description = "The ip for my current bastion"
+}
+
+variable "dns_records" {
+  description = "List of DNS A type records to be added"
+  type = map(object({
+    name = string
+    host = string
+  }))
+}
